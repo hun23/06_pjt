82114fa55d613640fba94f4b3bf13bced5c84025
hun23 <hunn2023@gmail.com>
2023-04-14 14:11:37 +0900

    accounts_skeleton_done

---
+++ accounts/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+# Create your models here.
+
+# custom user class
+class User(AbstractUser):
+    followings = models.ManyToManyField('self', related_name='followers')
