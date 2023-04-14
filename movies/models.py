1f8d433d7003e0c19a5849c803550a5d216c7942
ysparrk <youngseo0703@gmail.com>
2023-04-14 12:41:16 +0900

    movies_models_done

--- movies/models.py
+++ movies/models.py
@@ -1,9 +1,18 @@
 from django.db import models
-
+from accounts.models import User
 
 class Movie(models.Model):
     title = models.CharField(max_length=20)
     description = models.TextField()
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+    content = models.CharField(max_length=100)
+    movie_id = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.content
