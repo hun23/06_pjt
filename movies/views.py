ed79a44aa09b38e3eb470326ce68a84b87b00c1d
ysparrk <youngseo0703@gmail.com>
2023-04-14 15:49:38 +0900

    movies_views_fix_02

--- movies/views.py
+++ movies/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render, redirect
 from .models import Movie, Comment
 from .forms import MovieForm, CommentForm
-from django.views.decorators.http import require_POST
+from django.views.decorators.http import require_http_methods
 
-
+@require_http_methods(['GET'])
 def index(request):
     movies = Movie.objects.all()
     context = {
@@ -11,7 +11,7 @@ def index(request):
     }
     return render(request, 'movies/index.html', context)
 
-
+@require_http_methods(['GET', 'POST'])
 def create(request):
     if request.method == 'POST':
         form = MovieForm(request.POST)
@@ -25,7 +25,7 @@ def create(request):
     }
     return render(request, 'movies/create.html', context)
 
-
+@require_http_methods(['GET'])
 def detail(request, pk):
     movie = Movie.objects.get(pk=pk)
     context = {
@@ -33,13 +33,13 @@ def detail(request, pk):
     }
     return render(request, 'movies/detail.html', context)
 
-
+@require_http_methods(['POST'])
 def delete(request, pk):
     movie = Movie.objects.get(pk=pk)
     movie.delete()
     return redirect('movies:index')
 
-
+@require_http_methods(['GET','POST'])
 def update(request, pk):
     movie = Movie.objects.get(pk=pk)
     if request.method == 'POST':
@@ -56,13 +56,38 @@ def update(request, pk):
     return render(request, 'movies/update.html', context)
 
 # ===== 댓글 ======
-def comments_create(request):
-    pass
+# 1) 댓글 작성
+@require_http_methods(['POST'])
+def comments_create(request, pk):
+    movie = Movie.objects.get(pk=pk)
+    comment_form = CommentForm(request.POST)
+    if comment_form.is_valid():
+        comment = comment_form.save(commit=False)
+        comment.movie = movie
+        # 댓글 작성시 작성자 정보가 함께 저장
+        comment.user = request.user
+        comment.save()
+    return redirect('movies:detail', movie.pk)
 
+# 2) 댓글 삭제
+@require_http_methods(['POST'])
+def comments_delete(request, pk, comment_pk):
+    comment = Comment.objects.get(pk=comment_pk)
+    if request.user == comment.user: 
+        comment.delete()
+    return redirect('movies:detail', pk)
 
-def comments_delete(request):
-    pass
+# 3) like
+@require_http_methods(['POST'])
+def likes(request, movie_pk):
+    if request.user.is_authenticated: 
+        movie = Movie.objects.get(pk=movie_pk)
+        
+        if movie.like_users.filter(pk=request.user.pk).exists():
+            movie.like_users.remove(request.user)  
 
-
-def likes(request):
-    pass
+        else:
+            movie.like_users.add(request.user) 
+        return redirect('moives:index') 
+    
+    return redirect('movies:login')  
