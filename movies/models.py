f9c520493f36edab1e13b13e1f60e82e7736fa21
ysparrk <youngseo0703@gmail.com>
2023-04-14 13:42:50 +0900

    movies_models_fix1

--- movies/models.py
+++ movies/models.py
@@ -1,10 +1,11 @@
 from django.db import models
-from accounts.models import User
+from django.conf import settings
 
 class Movie(models.Model):
     title = models.CharField(max_length=20)
     description = models.TextField()
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='like_articles')
 
     def __str__(self):
         return self.title
@@ -12,7 +13,7 @@ class Movie(models.Model):
 class Comment(models.Model):
     content = models.CharField(max_length=100)
     movie_id = models.ForeignKey(Movie, on_delete=models.CASCADE)
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.content
